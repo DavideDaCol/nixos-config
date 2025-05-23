5458c6caa4148755558679729d1a08f3c7cdec05
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2025-05-24 00:59:02 +0200

    may the nix gods have mercy on me

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -66,5 +66,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion (tip: never)
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.11";
 }
